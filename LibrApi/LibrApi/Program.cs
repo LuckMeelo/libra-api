4050c0070c06e87a50d14d506ca456de53d9e568
Aman Menda <aman.menda@epitech.eu>
2025-03-04 13:11:01 +0100

    feat: add logger

--- LibrApi/LibrApi/Program.cs
+++ LibrApi/LibrApi/Program.cs
@@ -1,31 +1,51 @@
 using Microsoft.EntityFrameworkCore;
 using LibrApi.Data;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(
+        outputTemplate: "{HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}",
+        theme: SystemConsoleTheme.Colored)
+    .CreateLogger();
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<LibrApiDbContext>(options =>
-    options.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("librapi_db")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+    
+    builder.Services.AddSerilog();
+    builder.Services.AddControllers();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<LibrApiDbContext>(options =>
+        options.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("librapi_db")));
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
